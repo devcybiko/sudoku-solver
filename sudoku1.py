8fce6e0beedbf23c5719eac40b60a63e1c23e44b
Greg Smith <thinkable@thinkable.us>
2021-09-02 01:42:40 -0400

    tweaking termination criteria

--- sudoku1.py
+++ sudoku1.py
@@ -6,14 +6,13 @@ import time
 def solve(puzzle: Puzzle, row, col):
     next_row, next_col = puzzle.next_cell(row, col)
     candidates = puzzle.get_candidates(row, col)
+    orig_digit = puzzle.get(row, col)
     for candidate in candidates:
-        orig_digit = puzzle.get(row, col)
         puzzle.set(row, col, candidate)
-        if puzzle.is_solved(): return True
-        if next_row >= 9: continue
+        if next_row >= 9: return True
         solved = solve(puzzle, next_row, next_col)
         if solved: return True
-        puzzle.set(row, col, orig_digit)
+    puzzle.set(row, col, orig_digit)
     return False
 
 def main():
